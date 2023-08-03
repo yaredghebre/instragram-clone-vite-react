7fdb0b8ab04c46c7fb7a7fb8acf91b9986133839
yaredghebre <yared2391@gmail.com>
2023-08-03 18:24:59 -0300

    first commit

---
+++ src/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <div className="homepage-nav">
+        <h2>sidenav</h2>
+      </div>
+
+      <div className="homepage-timeline">
+        <h2>timeline</h2>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HelloWorld.css";
+
+function HelloWorld() {
+  return (
+    <div>
+      <h1>ciao</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
