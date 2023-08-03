e81ed472f2034b1eb4104fb18e1e3596e0d7aab0
yaredghebre <yared2391@gmail.com>
2023-08-03 19:57:32 -0300

    Fixing style in post

--- src/components/timeline/Posts.tsx
+++ src/components/timeline/Posts.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Posts.css";
 import { Avatar } from "@mui/material";
+import {
+  BookmarkBorderOutlined,
+  ChatBubbleOutline,
+  FavoriteBorderOutlined,
+  MoreHorizOutlined,
+  Telegram,
+} from "@mui/icons-material";
 
 function Posts() {
   return (
@@ -10,9 +17,29 @@ function Posts() {
           <Avatar>Y</Avatar>
           y.ar.o • <span>12h</span>
         </div>
+        <MoreHorizOutlined />
       </div>
-      <div className="post-image"></div>
-      <div className="post-footer"></div>
+
+      <div className="post-image">
+        <img
+          src="https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_640.png"
+          alt=""
+        />
+      </div>
+
+      <div className="post-footer">
+        <div className="post-footer-icons">
+          <div className="post-footer-icons-left">
+            <FavoriteBorderOutlined className="post-icon" />
+            <ChatBubbleOutline className="post-icon" />
+            <Telegram className="post-icon" />
+          </div>
+          <div className="post-footer-icons-right">
+            <BookmarkBorderOutlined className="post-icon" />
+          </div>
+        </div>
+        Liked by 182 people
+      </div>
     </div>
   );
 }
