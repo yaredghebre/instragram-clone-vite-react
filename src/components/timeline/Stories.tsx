8b6a46e8b978621d19853ef1bf911c7e14cd887a
yaredghebre <yared2391@gmail.com>
2023-08-04 13:21:55 -0300

    More structuring

--- src/components/timeline/Stories.tsx
+++ src/components/timeline/Stories.tsx
@@ -4,7 +4,7 @@ import { Avatar } from "@mui/material";
 
 function Stories() {
   return (
-    <div>
+    <div className="section-wrapper">
       <div className="stories-container">
         <div className="user-story">
           <Avatar>Y</Avatar>
@@ -30,6 +30,16 @@ function Stories() {
         <div className="user-story">
           <Avatar>Y</Avatar>
         </div>
+      </div>
+
+      <div className="user-logged">
+        <div className="user-info">
+          <Avatar>Y</Avatar>
+          <span className="user-name">y.ar.o</span>
+          <span className="user-relation">New to Instagram</span>
+        </div>
+
+        <div className="user-action">Log out</div>
       </div>
     </div>
   );
